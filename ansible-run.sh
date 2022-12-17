7cd0b0f5108f6e26fedf4e4a14c191516356d057
SHerlihy <65720475+SHerlihy@users.noreply.github.com>
2022-12-17 17:16:58 +0000

    Update ansible-run.sh

--- ansible-run.sh
+++ ansible-run.sh
@@ -1,9 +1,9 @@
 #!/usr/bim/env bash
 
 ## Install ansible
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update -y
-sudo apt-get install -y curl git software-properties-common ansible
+apt-add-repository -y ppa:ansible/ansible
+apt-get update -y
+apt-get install -y curl git software-properties-common ansible
 
 ## Pull my ansible
-sudo ansible-pull -U https://github.com/SHerlihy/ansilbe.git
+ansible-pull -U https://github.com/SHerlihy/ansible
