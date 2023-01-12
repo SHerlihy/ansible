be5c7bda613119c5d050e1fe215186d72f029d09
SHerlihy <steven_herlihy@yahoo.com>
2023-01-12 16:37:46 +0000

    update run

--- ansible-run.sh
+++ ansible-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update -y
-sudo apt-get install -y curl software-properties-common ansible
+apt-add-repository -y ppa:ansible/ansible
+apt-get update -y
+apt-get install -y curl software-properties-common ansible
 
 ## Pull my ansible
 ansible-pull -U https://github.com/SHerlihy/ansible
