b97845e4309558943994bbe688d08ad8f3451409
SHerlihy <steven_herlihy@yahoo.com>
2023-01-25 16:56:54 +0000

    Specify user as root

--- ansible-run.sh
+++ ansible-run.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+su root
+cd /usr/local/bin
+apt-get update
+apt-get upgrade -y
+apt-get install -y curl software-properties-common ansible git build-essential
+
 apt-add-repository -y ppa:ansible/ansible
 apt-get update -y
-apt-get install -y curl software-properties-common ansible
 
 ## Pull my ansible
-ansible-pull -U https://github.com/SHerlihy/ansible
+ansible-pull -U https://github.com/SHerlihy/ansible.git
